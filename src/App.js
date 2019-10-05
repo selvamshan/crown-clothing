e83558fb27715f8482e87dd3441108333e565077
Selvaraj <selvaraj_s4@hotmail.com>
2019-10-05 16:28:33 +0530

    update menu-itmes to navivate wihtRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
+
 class App extends Component {
   render() {
-    return <div className='App'>
-      <HomePage />
-    </div>;
+    return (
+      <div>
+        <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop/hats' component={HatsPage} />
+        </Switch>
+        </BrowserRouter>
+      </div>
+    );
 
   }    
   
